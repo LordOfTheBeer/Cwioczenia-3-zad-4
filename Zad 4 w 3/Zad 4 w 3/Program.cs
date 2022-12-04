677020220357f068011098821b00d2d47a2c5b95
LordOfTheBeer <116457854+LordOfTheBeer@users.noreply.github.com>
2022-12-04 09:02:06 +0100

    Add files via upload

---
+++ Zad 4 w 3/Zad 4 w 3/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Zad_4_w_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double s = 0;
+
+            for(n=0; n<=100; n++)
+            {
+                s += Math.Pow(-1, n ) / (2 * n + 1);
+            }
+            Console.WriteLine(s * 4);
+
+
+            
+        }
+    }
+}
